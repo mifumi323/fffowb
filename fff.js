5a4d825c8824d7297b8b01528f003f970be9021a
unknown <mifumi323@.(none)>
2011-09-26 09:16:27 +0900

    画像素材など

--- fff.js
+++ fff.js
@@ -3,8 +3,18 @@
 enchant('');
 
 window.onload = function() {
-    var game = new Game(320, 240);
-    game.onload = function() {
-    };
-    game.start();
+	var game = new Game(640, 480);
+	game.preload('back.png', 'main.png', 'needle.png');
+	game.fps = 30;
+	game.onload = function()
+	{
+		initgame();
+	};
+	game.start();
+	
+	function initgame()
+	{
+		f_x = f_y = 0.0;
+		f_dx = f_dy = 0.0;
+	}
 };
