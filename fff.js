abbbd21e58f7a333278b5a115110e3cccd11c62d
unknown <mifumi323@.(none)>
2011-09-27 09:21:06 +0900

    初期化処理など

--- fff.js
+++ fff.js
@@ -14,7 +14,15 @@ window.onload = function() {
 	
 	function initgame()
 	{
+		// 主人公
 		f_x = f_y = 0.0;
 		f_dx = f_dy = 0.0;
+		
+		// 敵
+		n_count = 0;
+		n_x = new Array();
+		n_y = new Array();
+		n_dx = new Array();
+		n_dy = new Array();
 	}
 };
