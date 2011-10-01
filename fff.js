58b50495f4d8cc5447fd375259f04be868fc0087
unknown <mifumi323@.(none)>
2011-10-01 16:41:35 +0900

    敵の動き追加

--- fff.js
+++ fff.js
@@ -2,20 +2,37 @@
 
 enchant('');
 
-window.onload = function() {
+window.onload = function()
+{
 	// 定数(のつもりの変数)
 	// タイルを並べる数(狭い幅方向。実際の画面サイズにより前後しうる)
 	var TILE_COUNT_NARROW = 15;
 	// FPS(ゲームスピード。多いと滑らかだが負荷が高い)
 	var FPS = 30;
+	// 1フレーム中何回移動処理を行うか(当たり判定の精度が上がる)
+	var FRAME_DIVISION = 1;
 	// 主人公の加速度
-	var FUNYA_ACCELERATION = 1.0;
+	var FUNYA_ACCELERATION = 900.0;
 	// 主人公の最高速
-	var FUNYA_MAXSPEED = 20.0;
+	var FUNYA_MAXSPEED = 600.0;
 	// 入力範囲の広さ(-1～1。ただし0を除く。0に近いときびきび動く)
 	var INPUT_AREA = 0.5;
 	// スクロールの追随率(1.0以下の正の数)
-	var SCROLL_RATE = 0.2;
+	var SCROLL_RATE = 0.141421356;
+	// 敵の加速度初期値
+	var NEEDLE_ACCELERATION_FIRST = FUNYA_ACCELERATION*0.5;
+	// 敵の加速度下限
+	var NEEDLE_ACCELERATION_MIN = FUNYA_ACCELERATION*0.5;
+	// 敵の加速度下限
+	var NEEDLE_ACCELERATION_MAX = FUNYA_ACCELERATION*1.2;
+	// 敵の最高速度初期値
+	var NEEDLE_MAXSPEED_FIRST = FUNYA_MAXSPEED*1.5;
+	// 敵の最高速度下限
+	var NEEDLE_MAXSPEED_MIN = FUNYA_MAXSPEED*0.8;
+	// 敵の最高速度下限
+	var NEEDLE_MAXSPEED_MAX = FUNYA_MAXSPEED*1.5;
+	// 敵の増殖時間
+	var NEEDLE_INCREASE_CYCLE = 5.0;
 
 	// 変数
 	var horizontal;
@@ -32,12 +49,19 @@ window.onload = function() {
 	var mapchip_offset;
 	var funya;
 	var needles;
+	var score;
+	var frame_time;
 
 	construct();
 	game.start();
 
 	function construct()
 	{
+		// 定数の補正
+		var innerfps = FPS*FRAME_DIVISION;
+		NEEDLE_INCREASE_CYCLE = Math.ceil(NEEDLE_INCREASE_CYCLE*innerfps);
+		frame_time = 1.0/innerfps;
+
 		// サイズ設定
 		horizontal = (window.innerWidth>=window.innerHeight);
 		if (horizontal) {
@@ -60,14 +84,14 @@ window.onload = function() {
 		game.fps = FPS;
 
 		// イベント
-		game.onload = initgame;
+		game.onload = startgame;
 		game.currentScene.addEventListener(enchant.Event.ENTER_FRAME, enterframe);
 		game.currentScene.addEventListener(enchant.Event.TOUCH_START, touchstart);
 		game.currentScene.addEventListener(enchant.Event.TOUCH_MOVE, touchmove);
 		game.currentScene.addEventListener(enchant.Event.TOUCH_END, touchend);
 	}
 
-	function initgame()
+	function startgame()
 	{
 		// マップ
 		map = new Map(tile_size, tile_size);
@@ -92,13 +116,25 @@ window.onload = function() {
 		funya.v_vx = funya.v_vy = 0.0;
 		funya.v_ax = funya.v_ay = 0.0;
 		funya.acceleration = FUNYA_ACCELERATION;
-		funya.friction = FUNYA_ACCELERATION/(FUNYA_ACCELERATION+FUNYA_MAXSPEED);
+		funya.limitter = getlimitter(FUNYA_ACCELERATION, FUNYA_MAXSPEED);
 		funya.onmove = funya_onmove;
 		funya.addEventListener(enchant.Event.RENDER, chara_render);
 		game.currentScene.addChild(funya);
 
 		// 敵
 		needles = new Array();
+		needles[0] = new Sprite(tile_size, tile_size);
+		needles[0].image = game.assets['needle.png'];
+		needles[0].v_x  = needles[0].v_y  = -200.0;
+		needles[0].v_vx = needles[0].v_vy = 0.0;
+		needles[0].v_ax = needles[0].v_ay = 0.0;
+		needles[0].acceleration = NEEDLE_ACCELERATION_FIRST;
+		needles[0].limitter = getlimitter(NEEDLE_ACCELERATION_FIRST, NEEDLE_MAXSPEED_FIRST);
+		needles[0].onmove = needle_onmove;
+		needles[0].addEventListener(enchant.Event.RENDER, chara_render);
+		game.currentScene.addChild(needles[0]);
+
+		score = 0;
 	}
 
 	function new_mapchip(x, y)
@@ -106,11 +142,57 @@ window.onload = function() {
 		return Math.floor(Math.random() * 2)*2+(x+y+mapchip_offset)%2;
 	}
 
+	function getlimitter(acceleration, maxspeed)
+	{
+		return 1.0-acceleration*frame_time/(acceleration*frame_time+maxspeed);
+	}
+
 	function enterframe(e)
 	{
 		updateinput();
-		funya_onmove();
+		for (var i=0; i<FRAME_DIVISION; i++) {
+			score++;
+			if (score%NEEDLE_INCREASE_CYCLE==0) {
+				increaseneedle();
+			}
+			funya.onmove();
+			for (var n=0; n<needles.length; n++) {
+				needles[n].onmove();
+			}
+		}
 		scroll();
+	}
+
+	function increaseneedle()
+	{
+		// 初期値設定
+		var new_n  = needles.length;
+		var rand_n = Math.floor(Math.random() * new_n);
+		var new_a  = NEEDLE_ACCELERATION_MIN+Math.random() * (NEEDLE_ACCELERATION_MAX-NEEDLE_ACCELERATION_MIN);
+		var rand_a = NEEDLE_ACCELERATION_MIN+Math.random() * (NEEDLE_ACCELERATION_MAX-NEEDLE_ACCELERATION_MIN);
+		var new_s  = NEEDLE_MAXSPEED_MIN+Math.random() * (NEEDLE_MAXSPEED_MAX-NEEDLE_MAXSPEED_MIN);
+		var rand_s = NEEDLE_MAXSPEED_MIN+Math.random() * (NEEDLE_MAXSPEED_MAX-NEEDLE_MAXSPEED_MIN);
+
+		// 新キャラ追加
+		needles[new_n] = new Sprite(tile_size, tile_size);
+		needles[new_n].image = game.assets['needle.png'];
+		needles[new_n].v_x  = needles[rand_n].v_x;
+		needles[new_n].v_y  = needles[rand_n].v_y;
+		needles[new_n].v_vx = -needles[rand_n].v_vy;
+		needles[new_n].v_vy = needles[rand_n].v_vx;
+		needles[new_n].v_ax = 0.0;
+		needles[new_n].v_ay = 0.0;
+		needles[new_n].acceleration = new_a;
+		needles[new_n].limitter = getlimitter(new_a, new_s);
+		needles[new_n].onmove = needle_onmove;
+		needles[new_n].addEventListener(enchant.Event.RENDER, chara_render);
+		game.currentScene.addChild(needles[new_n]);
+
+		// 前のやつも反動で動かす
+		needles[rand_n].v_vx = -needles[new_n].v_vx;
+		needles[rand_n].v_vy = -needles[new_n].v_vy;
+		needles[rand_n].acceleration = rand_a;
+		needles[rand_n].limitter = getlimitter(rand_a, rand_s);
 	}
 
 	function touchstart(e)
@@ -139,8 +221,8 @@ window.onload = function() {
 
 		if (mouse_on) {
 			// 仮想入力更新
-			mouse_v_x = (mouse_x - center_x)/narrow/INPUT_AREA;
-			mouse_v_y = (mouse_y - center_y)/narrow/INPUT_AREA;
+			mouse_v_x = (mouse_x - center_x)*2.0/narrow/INPUT_AREA;
+			mouse_v_y = (mouse_y - center_y)*2.0/narrow/INPUT_AREA;
 			mouse_v_vel = Math.sqrt(mouse_v_x*mouse_v_x+mouse_v_y*mouse_v_y);
 			if (mouse_v_vel>1.0) {
 				// 入力値飽和
@@ -156,12 +238,29 @@ window.onload = function() {
 
 	function funya_onmove()
 	{
-		funya.v_ax = mouse_v_x*funya.acceleration;
-		funya.v_ay = mouse_v_y*funya.acceleration;
-		funya.v_vx = (funya.v_vx+funya.v_ax)*(1.0-funya.friction);
-		funya.v_vy = (funya.v_vy+funya.v_ay)*(1.0-funya.friction);
-		funya.v_x += funya.v_vx;
-		funya.v_y += funya.v_vy;
+		this.v_ax = mouse_v_x*this.acceleration;
+		this.v_ay = mouse_v_y*this.acceleration;
+		updateposition(this);
+	}
+
+	function needle_onmove()
+	{
+		var dx = funya.v_x - this.v_x;
+		var dy = funya.v_y - this.v_y;
+		var d_2 = dx*dx+dy*dy;
+		if (d_2>0.0) {
+			this.v_ax = this.acceleration*dx/Math.sqrt(d_2);
+			this.v_ay = this.acceleration*dy/Math.sqrt(d_2);
+		}
+		updateposition(this);
+	}
+
+	function updateposition(obj)
+	{
+		obj.v_vx = (obj.v_vx+obj.v_ax*frame_time)*obj.limitter;
+		obj.v_vy = (obj.v_vy+obj.v_ay*frame_time)*obj.limitter;
+		obj.v_x += obj.v_vx*frame_time;
+		obj.v_y += obj.v_vy*frame_time;
 	}
 
 	function chara_render(e)
@@ -185,6 +284,11 @@ window.onload = function() {
 
 		funya.v_x -= s_x;
 		funya.v_y -= s_y;
+
+		for (var n=0; n<needles.length; n++) {
+			needles[n].v_x -= s_x;
+			needles[n].v_y -= s_y;
+		}
 
 		map.v_x -= s_x;
 		map.v_y -= s_y;
